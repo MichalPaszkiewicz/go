3e7a7826b3761ab9da8459a53b2073c26c5acdbe
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-08-04 13:05:27 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -3,6 +3,12 @@ function aiMove()
   var aiPlayer = otherValue(player);
   var legalMoves = getLegalMoves(aiPlayer);
   
+  if(legalMoves.length == 0)
+  {
+  	gameOver();
+  	return;
+  }
+  
   var i = Math.floor(legalMoves.length * Math.random());
   
  var x = legalMoves[i].xPos;
