7dc2f4502e16e7fa2cfeddf238d6a8b9d1aeabfa
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-08-05 12:36:26 +0100

    Update display-updater.js

--- display-updater.js
+++ display-updater.js
@@ -18,20 +18,20 @@ function updateDisplay()
   
   if(player == vWhite)
   {
-    $("#player-colour").text("You are white").css({ color : "white"});
+    $("#player-colour").text("You are white").css({ color : "white", text-shadow : "1px 1px black"});
   }
   if(player == vBlack)
   {
-    $("#player-colour").text("You are black").css({ color : "black"});
+    $("#player-colour").text("You are black").css({ color : "black", text-shadow : "none"});
   }
   
   if(currentTurn == vWhite)
   {
-    $("#move-notifier").text("White to move");
+    $("#move-notifier").text("White to move").css({ color : "white", text-shadow : "1px 1px black"});
   }
   else
   {
-    $("#move-notifier").text("Black to move");    
+    $("#move-notifier").text("Black to move").css({ color : "black", text-shadow : "none"});    
   }
 }
 
